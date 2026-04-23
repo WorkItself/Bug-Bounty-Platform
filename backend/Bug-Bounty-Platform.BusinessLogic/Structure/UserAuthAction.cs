d53198ddb4dae2e763a1aaa13e5b119531f1fdd2
Fl1xen21 <maxim.mirzac@isa.utm.md>
2026-04-23 12:50:39 +0300

    feat: implement JWT authentication with signing key

--- backend/Bug-Bounty-Platform.BusinessLogic/Structure/UserAuthAction.cs
+++ backend/Bug-Bounty-Platform.BusinessLogic/Structure/UserAuthAction.cs
@@ -1,12 +1,13 @@
 using Bug_Bounty_Platform.BusinessLogic.Core;
 using Bug_Bounty_Platform.BusinessLogic.Interfaces;
 using Bug_Bounty_Platform.Domain.Models.User;
+using Microsoft.Extensions.Configuration;
 
 namespace Bug_Bounty_Platform.BusinessLogic.Structure
 {
     public class UserAuthAction : UserActions, IUserLoginAction
     {
-        public UserAuthAction() { }
+        public UserAuthAction(IConfiguration configuration) : base(configuration) { }
 
         public object UserLoginDataValidation(UserLoginDto udata)
         {
